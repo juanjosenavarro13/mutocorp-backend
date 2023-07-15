6d77670aad2c65e680193d046b70e7fd9b471e1b
juanxo13597 <juanxo97master@gmail.com>
2023-07-15 18:37:01 +0200

    fix import

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { Role } from '../types';
-import { Raider } from 'src/raiders';
+import { Raider } from '../../raiders';
 
 export type UserDocument = HydratedDocument<User>;
 
